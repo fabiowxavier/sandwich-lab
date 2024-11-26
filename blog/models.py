0517fa286ac7b67b50304a7097a6be32546b37e5
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-26 15:39:42 +0000

    added class meta and def to post class

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,12 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True) 
     status = models.IntegerField(choices=STATUS, default=0) 
     excerpt = models.TextField(blank=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
