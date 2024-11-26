c3fcd1c3890747724f61cde72455e9454afb0711
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-26 16:22:38 +0000

    fix bug with admin posts and comments]

--- blog/models.py
+++ blog/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model): 
     title = models.CharField(max_length=200, unique=True) 
     slug = models.SlugField(max_length=200, unique=True) 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts") 
-    content = models.TextField() 
+    author = models.ForeignKey( User, on_delete=models.CASCADE, related_name="blog_posts" ) 
+    content = models.TextField(default="Default content") 
     created_on = models.DateTimeField(auto_now_add=True) 
     status = models.IntegerField(choices=STATUS, default=0) 
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True) 
+    updated_on = models.DateTimeField(auto_now=True) 
     
-    class Meta:
-        ordering = ["-created_on"]
-        
-    def __str__(self):
+    class Meta: 
+        ordering = ["-created_on"] 
+            
+    def __str__(self): 
         return f"{self.title} | written by {self.author}"
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80, default="Anonymous")
+    email = models.EmailField(default="default@example.com")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
