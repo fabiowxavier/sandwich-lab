a57a22a106ac47b1236850b52257eb169332cb09
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-27 13:41:26 +0000

    add location field to post model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0) 
     excerpt = models.TextField(blank=True) 
     updated_on = models.DateTimeField(auto_now=True) 
+    location = models.CharField(max_length=100)
     
     class Meta: 
         ordering = ["-created_on"] 
