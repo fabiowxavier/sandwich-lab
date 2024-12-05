a0020d6a98a8c3e1fbea900fdb2e23d1f9a1f6ed
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-12-05 10:44:46 +0000

    fix bugs alt issues on post details and change comments.js loops

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = '1' in os.environ
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-fabiowxavie-sandwichlab-leiztp7srq2.ws.codeinstitute-ide.net',  
     'sandwich-lab-7fc0ba3ca59c.herokuapp.com',  
